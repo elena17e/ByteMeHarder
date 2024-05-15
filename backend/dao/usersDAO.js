11f568f39767a3e8b5c6ac535b7e540e1f3c1e01
Sebastian Arbutina <sarbutina20@gmail.com>
2024-05-15 20:30:38 +0200

    Added a lot of logic for communication with db.

--- backend/dao/usersDAO.js
+++ backend/dao/usersDAO.js
@@ -1,49 +1,58 @@
-
 const bcrypt = require("bcrypt");
-const db = require('../db');
-const xss = require('xss-clean'); 
+const db = require("../db");
+const xss = require("xss-clean");
 
 const registerUser = async (req, res) => {
   const { name, surname, email, password } = req;
-  try {
-    
+
     const safeName = xss(name);
     const safeSurname = xss(surname);
     const safeEmail = xss(email);
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "+ safeEmail);
 
-    
-    const [existingUser] = await db.query("SELECT * FROM users WHERE email = ?", [safeEmail]);
+    const [existingUser] = await db.query(
+      "SELECT * FROM user WHERE email = ?",
+      [safeEmail]
+    );
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "+ existingUser);
+
     if (existingUser.length > 0) {
       return res.status(400).json({ message: "Email already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await db.query("INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)", [safeName, safeSurname, safeEmail, hashedPassword]);
+    await db.query(
+      "INSERT INTO user (name, surname, email, password) VALUES (?, ?, ?, ?)",
+      [safeName, safeSurname, safeEmail, hashedPassword]
+    );
+    
 
-    const [newUser] = await db.query("SELECT * FROM users WHERE email = ?", [safeEmail]);
-
-    res.status(201).json({ message: "User registered successfully", user: newUser[0] });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+    
+    const [newUser] = await db.query("SELECT * FROM user WHERE email = ?", [
+      safeEmail,
+    ]);
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!User registered successfully");
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser[0] });
 };
 
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
-    
     const safeEmail = xss(email);
 
-    
-    const [users] = await db.query("SELECT * FROM users WHERE email = ?", [safeEmail]);
+    const [users] = await db.query("SELECT * FROM user WHERE email = ?", [
+      safeEmail,
+    ]);
     const user = users[0];
     if (!user) {
-      return res.status(404).json({ message: "User not found or invalid credentials" });
+      return res
+        .status(404)
+        .json({ message: "User not found or invalid credentials" });
     }
 
-   
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
       return res.status(401).json({ message: "Invalid credentials" });
@@ -59,17 +68,15 @@ const loginUser = async (req, res) => {
 const forgotPassword = async (req, res) => {
   const { email } = req.body;
   try {
-    
     const safeEmail = xss(email);
 
-    
-    const [users] = await db.query("SELECT * FROM users WHERE email = ?", [safeEmail]);
+    const [users] = await db.query("SELECT * FROM user WHERE email = ?", [
+      safeEmail,
+    ]);
     const user = users[0];
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
-
 
     res.json({ message: "Password reset successful" });
   } catch (error) {
@@ -83,4 +90,3 @@ module.exports = {
   loginUser,
   forgotPassword,
 };
-
