d3a64fe3883da62fb557a37272a6394ab560d121
Sebastian Arbutina <sarbutina20@gmail.com>
2024-05-15 21:37:01 +0200

    Finished login, registration and tested successful process.

--- backend/dao/usersDAO.js
+++ backend/dao/usersDAO.js
@@ -1,56 +1,49 @@
 const bcrypt = require("bcrypt");
-const db = require('../db');
+const db = require("../db");
 
 const registerUser = async (req, res) => {
-  const { name, surname, email, password } = req.body;
-  try {
-    const [existingUser] = await db.query(
-      "SELECT * FROM users WHERE email = ?",
-      [email]
-    );
-    if (existingUser.length > 0) {
-      return res.status(400).json({ message: "Email already exists" });
-    }
+  const { name, surname, email, password } = req;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    await db.query(
-      "INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)",
-      [name, surname, email, hashedPassword]
-    );
-
-    const [newUser] = await db.query("SELECT * FROM users WHERE email = ?", [
-     email,
-    ]);
-
-    res
-       .status(201)
-       .json({ message: "User registered successfully", user: newUser[0] });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
+  const [existingUser] = await db.query("SELECT * FROM user WHERE email = ?", [
+    email,
+  ]);
+  if (existingUser.length > 0) {
+    return 401;
   }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await db.query(
+    "INSERT INTO user (name, surname, email, password) VALUES (?, ?, ?, ?)",
+    [name, surname, email, hashedPassword]
+  );
+
+  const [newUser] = await db.query("SELECT * FROM user WHERE email = ?", [
+    email,
+  ]);
+
+  return newUser[0];
 };
 
 const loginUser = async (req, res) => {
   console.log(req);
   const { email, password } = req;
   try {
-    const [users] = await db.query("SELECT * FROM users WHERE email = ?", [
-       email,
+    const [users] = await db.query("SELECT * FROM user WHERE email = ?", [
+      email,
     ]);
     const user = users[0];
     if (!user) {
       return res
-      .status(404)
-      .json({ message: "User not found or invalid credentials" });
+        .status(404)
+        .json({ message: "User not found or invalid credentials" });
     }
-  const passwordMatch = await bcrypt.compare(password, user.password);
+    const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      return 400;
     }
-
-    res.json({ message: "Login successful", user: user });
+    return user;
+    
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server Error" });
@@ -58,10 +51,10 @@ const loginUser = async (req, res) => {
 };
 
 const forgotPassword = async (req, res) => {
-  const { email } = req.body;
+  const { email } = req;
   try {
-    const [users] = await db.query("SELECT * FROM users WHERE email = ?", [
-       email,
+    const [users] = await db.query("SELECT * FROM user WHERE email = ?", [
+      email,
     ]);
     const user = users[0];
     if (!user) {
--- backend/index.js
+++ backend/index.js
@@ -23,19 +23,25 @@ app.get('/events/:url', verifyToken, eventsDAO.searchMaliciousURL);
 //app.put('/events/:id/comments/:commentId', eventsDAO.updateEventComment);
 
 app.post('/register', async (req, res) => {
-    const result = usersDAO.registerUser(req.body);
+    const result = await usersDAO.registerUser(req.body, res);
+    if(result === 401) {
+        return res.status(401).json({ message: "User already exists" });
+    }
     const token = jwt.sign({ name: result.name, surname: result.surname, email: result.email }, process.env.JWT_SECRET_KEY);
     res.json({ token });
 });
 
 app.post('/login', async (req, res) => {
-    const result = usersDAO.loginUser(req.body);
-    const token = jwt.sign({ name: result.user.name, surname: result.user.surname, email: result.user.email }, process.env.JWT_SECRET_KEY);
+    const result = await usersDAO.loginUser(req.body, res);
+    if(result === 401) {
+        res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ name: result.name, surname: result.surname, email: result.email }, process.env.JWT_SECRET_KEY);
     res.json({ token });
 });
 
 app.post('/forgot-password', async (req, res) => {
-    const result = usersDAO.forgotPassword(req.body);
+    const result = await usersDAO.forgotPassword(req.body, res);
     //const token = jwt.sign({ /* user data */ }, JWT_SECRET_KEY);
     res.json({ result });
 });
